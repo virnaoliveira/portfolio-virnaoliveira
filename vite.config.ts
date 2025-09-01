4cf8b1083ef82dba78901fc015dbd1dcb1447a82
Virna Oliveira <92312347+virnaoliveira@users.noreply.github.com>
2025-09-01 19:53:49 -0300

    correções

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-virnaoliveira/', 
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
